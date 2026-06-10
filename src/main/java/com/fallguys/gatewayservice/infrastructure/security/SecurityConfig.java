95aeb311e37ed33a2f32f089d0fdb57085674ec2
yys7517 <yys7517@naver.com>
2026-06-10 09:52:33 +0900

    feature(ERP-198): 로그인 성공 시, 로컬 프론트엔드 리다이렉트 설정, CORS 설정 (#11)

--- src/main/java/com/fallguys/gatewayservice/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/fallguys/gatewayservice/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fallguys.gatewayservice.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,12 +16,20 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            AuthenticationSuccessHandler oauth2AuthenticationSuccessHandler
+    ) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
@@ -30,19 +39,22 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 ->
-                        oauth2.successHandler(oauth2AuthenticationSuccessHandler())
+                        oauth2.successHandler(oauth2AuthenticationSuccessHandler)
                 )
                 .oauth2Client(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationSuccessHandler oauth2AuthenticationSuccessHandler() {
+    public AuthenticationSuccessHandler oauth2AuthenticationSuccessHandler(
+            @Value("${app.frontend-base-url}") String frontendBaseUrl
+    ) {
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         successHandler.setRequestCache(new HttpSessionRequestCache());
-        successHandler.setDefaultTargetUrl("/api/users/test");
+        successHandler.setDefaultTargetUrl(frontendBaseUrl);    // React Home 화면으로 Redirect
         return successHandler;
     }
 
@@ -61,5 +73,18 @@ public class SecurityConfig {
         );
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
         return authorizedClientManager;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
