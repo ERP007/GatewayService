5e6d27f5f84ed06a2f537d9e7644bd1ab76a1874
yys7517 <yys7517@naver.com>
2026-06-23 18:06:07 +0900

    feature(ERP-285): @Positive 제약을 검증하기 위한 @Valid 설정 (#24)

--- src/main/java/com/fallguys/gatewayservice/infrastructure/messaging/SessionInvalidationMessagingProperties.java
+++ src/main/java/com/fallguys/gatewayservice/infrastructure/messaging/SessionInvalidationMessagingProperties.java
@@ -1,5 +1,6 @@
 package com.fallguys.gatewayservice.infrastructure.messaging;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -22,7 +23,7 @@ public record SessionInvalidationMessagingProperties(
         @NotBlank String deadLetterExchange,
         @NotBlank String deadLetterQueue,
         @NotBlank String deadLetterRoutingKey,
-        @NotNull DlqRedrive dlqRedrive
+        @NotNull @Valid DlqRedrive dlqRedrive
 ) {
 
     public record DlqRedrive(
